0dbf53578168e0c88592acb1559d615c260cc7fb
황진홍 <114119061+shrk-sh-ioai@users.noreply.github.com>
2022-12-05 13:11:31 +0700

    update

---
+++ laporan/TB_01.py
@@ -0,0 +1,5 @@
+#import library
+import pandas as pd;
+import numpy as np;
+
+mall = pd.read_csv("https://raw.githubusercontent.com/shrk-sh-ioai/tubes-ppdp/main/mall_customer_dt/Mall_Customers.csv", sep=",", encoding='cp1252');
