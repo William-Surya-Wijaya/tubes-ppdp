daca89885a6fb19da6529d8fde9f8274ca4c50a7
황진홍 <william1412.surya@gmail.com>
2022-12-07 09:28:14 +0700

    update

--- laporan/TB_01.py
+++ laporan/TB_01.py
@@ -2,4 +2,10 @@
 import pandas as pd;
 import numpy as np;
 
-mall = pd.read_csv("https://raw.githubusercontent.com/shrk-sh-ioai/tubes-ppdp/main/mall_customer_dt/Mall_Customers.csv", sep=",", encoding='cp1252');
+#load data
+mall = pd.read_csv("https://raw.githubusercontent.com/shrk-sh-ioai/tubes-ppdp/main/mall-customer-dt/mall-customers.csv", sep=",", encoding='cp1252');
+print(mall);
+
+#checking & cleaning data
+dfCheck = mall[mall.isna().any(axis=1)];
+print(dfCheck);
