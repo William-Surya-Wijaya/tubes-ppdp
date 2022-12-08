8e33827f6cc37ac703c964b1682c8520d1afa5af
황진홍 <william1412.surya@gmail.com>
2022-12-08 09:33:31 +0700

    up

--- laporan/TB_01.py
+++ laporan/TB_01.py
@@ -4,8 +4,10 @@ import numpy as np;
 
 #load data
 mall = pd.read_csv("https://raw.githubusercontent.com/shrk-sh-ioai/tubes-ppdp/main/mall-customer-dt/mall-customers.csv", sep=",", encoding='cp1252');
-print(mall);
 
 #checking & cleaning data
 dfCheck = mall[mall.isna().any(axis=1)];
-print(dfCheck);
+print("Data with NaN values :"); print(dfCheck);
+
+print("Data with minimum values :"); print(mall.min());
+print("Data with maximum values :"); print(mall.max());
